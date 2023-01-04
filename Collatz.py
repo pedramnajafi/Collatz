a05ffe91ade18d982e1d3f311fea221433e5be5b
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-01-04 18:58:30 +0330

    Add files via upload

---
+++ Collatz.py
@@ -0,0 +1,15 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+        
+try:
+    number_inserted = input('Please enter a number ')
+    while number_inserted != 1:
+        number_inserted = collatz(int(number_inserted))
+except ValueError:
+    print('you must enter a number')    
